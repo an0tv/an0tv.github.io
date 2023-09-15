39f125bfbd8ac2935276f93b8f9d828241cd3d47
Andrey <andreyotvagingrey@gmail.com>
2023-09-15 10:45:25 -0700

    added adaptive ThreeJS test code

---
+++ src/components/test.vue
@@ -0,0 +1,50 @@
+<script setup>
+import * as THREE from 'three';
+
+//sizes of window for window optimization 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+window.addEventListener('resize', () => {
+    //update size
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update camera
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+})
+
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
+
+	renderer.render( scene, camera );
+}
+
+animate()
+</script>
