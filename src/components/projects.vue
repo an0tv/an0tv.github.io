fd6056b94facef462c212df03f324bca6bccba83
Andrey <andreyotvagingrey@gmail.com>
2023-09-29 11:29:21 -0700

    boiler plate

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -1,4 +1,37 @@
 <template>
+  <head>
+    <link type="stylesheet" href="../style.css">
+  </head>
   <h1>Project Page</h1>
+  <div class="row">
+    <div class="col">
+      <div class="container">
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+      </div>
+      <div class="container">
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+      </div>
+      <div class="container">
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+      </div>
+      <div class="container">
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+      </div>
+      <div class="container">
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+        <div class="item">Test</div>
+      </div>
+      
+  </div>
+</div>
 </template>
 
