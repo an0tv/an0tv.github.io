a52f00d7a650d5eb896fb27daef15570af983318
Andrey <andreyotvagingrey@gmail.com>
2023-10-01 12:45:54 -0700

    added more boiler platge

--- src/components/projects.vue
+++ src/components/projects.vue
@@ -6,31 +6,70 @@
   <div class="row">
     <div class="col">
       <div class="container">
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-        <div class="item">Test</div>
+        <div class="item">
+          <h1> Picture of Project </h1>
+        </div>
+        <div class="item_desc">
+          <h2>
+            Description of Project
+          </h2>
+          <h2>
+            Link to project page?
+          </h2>
+        </div>
+      </div>      
+      <div class="container">
+        <div class="item">
+          <h1> Picture of Project </h1>
+        </div>
+        <div class="item_desc">
+          <h2>
+            Description of Project
+          </h2>
+          <h2>
+            Link to project page?
+          </h2>
+        </div>
       </div>
       <div class="container">
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-        <div class="item">Test</div>
+        <div class="item">
+          <h1> Picture of Project </h1>
+        </div>
+        <div class="item_desc">
+          <h2>
+            Description of Project
+          </h2>
+          <h2>
+            Link to project page?
+          </h2>
+        </div>
       </div>
       <div class="container">
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-        <div class="item">Test</div>
+        <div class="item">
+          <h1> Picture of Project </h1>
+        </div>
+        <div class="item_desc">
+          <h2>
+            Description of Project
+          </h2>
+          <h2>
+            Link to project page?
+          </h2>
+        </div>
       </div>
       <div class="container">
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-        <div class="item">Test</div>
+        <div class="item">
+          <h1> Picture of Project </h1>
+        </div>
+        <div class="item_desc">
+          <h2>
+            Description of Project
+          </h2>
+          <h2>
+            Link to project page?
+          </h2>
+        </div>
       </div>
-      <div class="container">
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-        <div class="item">Test</div>
-      </div>
-      
   </div>
 </div>
 </template>
