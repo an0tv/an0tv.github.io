7f31e21b9a458205697e490b722cd86b0499bbad
Andrey <andreyotvagingrey@gmail.com>
2023-12-07 13:42:00 -0800

    i hate html and css but styling is a little better

--- src/components/projects2.vue
+++ src/components/projects2.vue
@@ -9,6 +9,7 @@ import { createRenderer } from 'vue';
     <center>
     <h1>Projects</h1>
   </center>
+
     <h2>
       1. Gaucho Racing cloud telemetry 2024
     </h2>
@@ -19,7 +20,7 @@ import { createRenderer } from 'vue';
         <img src="../assets/react.png" style="max-height:185px; height: auto; width: 33%; max-width: 200px;">
     </div>
     
-    <p style="word-wrap: break-word;width: 75%; max-width: 800px;">
+    <p style="margin-right:auto; margin-left: auto; word-wrap: break-word; max-width: 600px;">
       Cloud telemetry system using an LTE module to collect CAN frames and send them to an MQTT ingest server for decryption and realtime 
       analysis. MQTT ingest server is connected to a SingleStore db for data storage and later analysis. Realtime data is sent from ingest server 
       to React App for realtime telemetry. 
@@ -30,7 +31,7 @@ import { createRenderer } from 'vue';
     </h2>
     <div class="imrow"><img src="../assets/wheel.png" style="max-height:185px; height: auto; width: 100%; max-width: 185px;"></div>
     
-    <p style="word-wrap: break-word;width: 75%; max-width: 800px;">
+    <p style="margin-right:auto; margin-left: auto; word-wrap: break-word; max-width: 600px;">
       A dashboard that is integrated into the UCSB GR23 steering wheel.
       Screen runs on a teensy 4.1 and has a 6 axis gyroscope for accurate steering wheel positioning. 
       Displays speed by using a CAN bus connection and returns positioning back via CAN bus. Main from of 
